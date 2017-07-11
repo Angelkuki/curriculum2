8a2d0bab51a52a1fa89460069cc497069d7a2462
Angelkuki <kukichingon@gmail.com>
2017-07-11 18:23:27 -0500

    curriculum2

---
+++ scripPrincipal2.js
@@ -0,0 +1,8 @@
+$(documento).on("ready",configurarApp);
+
+function configurarApp ()
+{
+    var canvas = document.getElementById("miCanvas");
+    var  ctx = canvas.getContext("2d");
+    canvas.width = screen.availWidth;
+}
